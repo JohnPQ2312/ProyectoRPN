f7c6e9544bcb068b1428cfec967422d16e94907d
JOHN PEREZ QUESADA <john.perez.quesada@est.una.ac.cr>
2024-08-18 00:17:55 -0600

    BETA V0.9.1

--- ProyectoRPN/DivisionFunction.h
+++ ProyectoRPN/DivisionFunction.h
@@ -1,14 +1,18 @@
 #pragma once
+#include "VerifyNumber.h"
 
 using namespace std;
 
 float DivisionFunction(float FirstValue, float SecondValue)
 {
-	if (SecondValue == 0) {
-		cout << "No se puede dividir entre 0 \n";
-		return FirstValue;
-	}
-	else {
-		return FirstValue / SecondValue;
-	}
+    while (SecondValue == 0) {
+        cout << "No se puede dividir entre 0. Por favor, ingrese un valor diferente: ";
+		cin >> SecondValue;
+        while (VerifyNumber(SecondValue) == true) {
+            cout << "Ingrese el segundo valor: ";
+            cin >> SecondValue;
+        }
+    }
+
+    return FirstValue / SecondValue;
 }
--- ProyectoRPN/VerifyNumber.h
+++ ProyectoRPN/VerifyNumber.h
@@ -6,7 +6,13 @@ bool VerifyNumber(float number) {
 	if (cin.fail()) {
 		cin.clear();
 		cin.ignore(1000000, '\n');
-		cout << "Error, ingrese un numero valido \n";
+		cout << "Dato invalido, ingrese un numero valido \n";
+		return true;
+	}
+	else if (cin.peek() == ',') {
+		cin.clear();
+		cin.ignore(1000000, '\n');
+		cout << "Dato invalido, el uso de coma no es valido para decimales, use el punto en su lugar \n";
 		return true;
 	}
 	else {
